3926d6d758b7abe59e70c8fdd88db678a1c1eebe
Dwayne Macgowan <dwaynemac@gmail.com>
2026-03-30 13:10:50 -0300

    Revert "add jetbrains-rules CLI and installation process"
    This reverts commit fea65eba8d693f06f74ca04c128c09d582821e89.

--- bin/cli.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { runCli } from '../lib/cli.mjs';
-
-await runCli(process.argv.slice(2), {
-  cwd: process.cwd(),
-  stdout: process.stdout,
-  stderr: process.stderr,
-});
